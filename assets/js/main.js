de7de7b8f767b61d246672f0ac7f1c65c419e629
Misstaqe <jsonfreel@gmail.com>
2021-08-30 11:28:25 +1000

    Fixed displaying clock in header

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,8 +155,8 @@
 		});
 
 		// initialize live clock
-		// const clock = new Clock();
-		// clock.start();
+		const clock = new Clock();
+		clock.start();
 
 		// invest package select
 		var planIcon = $(".plan-icon");
